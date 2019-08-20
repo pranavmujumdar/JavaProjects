250d1049a3eb05dfde00d808665f9d951a7db7c9
Pranav Mujumdar <40158654+pranavmujumdar@users.noreply.github.com>
2019-08-20 12:43:30 -0400

    Hello World
    Hello World and Int and basic operators

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,15 @@
+public class Hello {
+
+    public static void main(String[] args) {
+        int aNumber = 5;
+        int secondNumber = 12;
+        int thirdNumber = aNumber * 2;
+        int myTotal = aNumber + secondNumber + thirdNumber;
+        int lastOne = 1000 - myTotal;
+
+        System.out.println("Hello World Date 08/20/2019");
+        System.out.println("Declared a Number "+ aNumber);
+        System.out.println("Total of my numbers: "+ myTotal);
+        System.out.println(lastOne);
+    }
+}
