cda680a3932b36482d4fc0ae009fccfccfd5b4bf
Pranav Mujumdar <40158654+pranavmujumdar@users.noreply.github.com>
2019-08-26 11:19:18 -0400

    RevisionAndStrings

---
+++ RecapAndString/src/com/pranavmujumdar/Main.java
@@ -0,0 +1,31 @@
+package com.pranavmujumdar;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        /*all the data types
+            i. byte
+            ii. short
+            iii. int
+            iv. long
+            v. float
+            vi. double
+            vii. char
+            viii. boolean
+
+            mostly used will be int, double, boolean
+
+         */
+
+
+        //String is not a Data type it's a Fucking Class, String is a sequence of characters,
+        // it could contain a single character(regular or unicode) or it could contain a sequence as well
+        // max value of string is 2.14 Billion
+
+        String myStringUnicode = "\u0092"; //refer the unicode table to get the value of the unicode unicode-table.com
+        String myString = "Sequence of characters and numbers 4841148sdf7f";
+        System.out.println(myStringUnicode);
+        System.out.println(myString);
+    }
+}
